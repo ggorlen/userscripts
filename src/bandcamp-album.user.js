08361cf1858100fd04874695e2935bb838c64ab9
ggorlen <gsgorlen@gmail.com>
2022-01-15 11:38:16 -0800

    basic controls

--- src/bandcamp-album.user.js
+++ src/bandcamp-album.user.js
@@ -2,7 +2,7 @@
 // @name         Bandcamp album userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  block noisy UI on bandcamp album pages
+// @description  block noisy UI on bandcamp album pages, add key controls
 // @author       ggorlen
 // @run-at       document-start
 // @match        https://*.bandcamp.com/album*
@@ -55,5 +55,30 @@
   })();
 
   document.addEventListener("DOMContentLoaded", () => {
+    const tracks = [...document.querySelectorAll(".track_row_view")];
+    document.addEventListener("keydown", e => {
+      // TODO add skip ahead/back with arrow left/right
+      if (e.code === "Comma") {
+        let i = tracks.indexOf(document.querySelector(".track_row_view.current_track"));
+
+        if (i >= 0) {
+          const prevTrack = i - 1 < 0 ? tracks.length - 1 : i - 1;
+          tracks[prevTrack].querySelector(".play_status").click();
+        }
+      }
+      else if (e.code === "Period") {
+        let i = tracks.indexOf(document.querySelector(".track_row_view.current_track"));
+
+        if (i >= 0) {
+          const nextTrack = ++i % tracks.length;
+          tracks[nextTrack].querySelector(".play_status").click();
+        }
+      }
+      else if (e.code === "Space") {
+        e.preventDefault();
+        document.querySelector(".playbutton")?.click();
+      }
+    });
   });
 })();
+
