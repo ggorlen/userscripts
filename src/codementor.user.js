f77e0c8b83b936483934c488897e833cad086700
ggorlen <gsgorlen@gmail.com>
2024-04-02 08:00:44 -0700

    Remove banner and filter

--- src/codementor.user.js
+++ src/codementor.user.js
@@ -9,10 +9,39 @@
 // @grant        none
 // ==/UserScript==
 
+const pollUntilExists = (sel, cb, stopAfter = 10_000, pollMs = 200) => {
+  const interval = setInterval(() => {
+    const els = document.querySelectorAll(sel);
+
+    if (els.length) {
+      clearInterval(interval);
+      cb(els);
+    }
+  }, pollMs);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    console.warn(`Unable to find selector ${sel} after ${stopAfter}ms`);
+  }, stopAfter);
+};
+
 (function () {
   "use strict";
 
+  // get new listings every 60 seconds
   setInterval(() => {
-    document.querySelector(".request-filter__refresh-btn").click();
-  }, 60000);
+    document.querySelector(".request-filter__refresh-btn")?.click();
+  }, 60_000);
+
+  // remove the ARC advertisement banner
+  pollUntilExists(
+    '[data-track="cm-openRequest-arcBanner"]',
+    els => els.forEach(el => el.remove())
+  );
+
+  // remove filtering by my interests (I want to see all requests)
+  pollUntilExists(
+    ".request-filter__active-filter",
+    els => els[0].click()
+  );
 })();
