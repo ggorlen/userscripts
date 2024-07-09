cda869d8ed2d78b9b059b4b9f881f1a65fb4d481
ggorlen <gsgorlen@gmail.com>
2024-07-09 10:20:09 -0700

    Improve bandcamp feed denoiser

--- src/bandcamp-feed.user.js
+++ src/bandcamp-feed.user.js
@@ -5,7 +5,7 @@
 // @description  Remove crap from BC feed
 // @author       ggorlen
 // @run-at       document-start
-// @match        https://bandcamp.com/ggorlen/feed
+// @match        https://bandcamp.com/ggorlen/feed?*
 // @icon         https://www.google.com/s2/favicons?domain=bandcamp.com
 // @grant        none
 // ==/UserScript==
@@ -16,22 +16,34 @@
      .. I guess the sidebar already does this, but the sequence is sort of unclear
 */
 (() => {
-  const startTime = Date.now();
-  (function check() {
-    [...document.querySelectorAll("#story-list .story")].forEach(e => {
-      const deny = ["started following you", "bought", "pre-order"];
-
-      if (deny.some(s => e.innerText.includes(s))) {
+  const removeBuyNow = () => {
+    [...document.querySelectorAll("li")].forEach(e => {
+      if (
+        e.textContent.trim().toLowerCase() === "buy now"
+      ) {
         e.remove();
       }
     });
-    const fans = document.querySelector(".suggested-fans");
-    fans && (fans.innerText = "");
-    const rel = document.querySelector(".more-releases");
-    rel?.offsetParent && rel.click();
+  };
+
+  const deny = ["started following you", "bought", "pre-order"];
+
+  const startTime = Date.now();
+  (function poll() {
+    [...document.querySelectorAll("#story-list .story")].forEach(e => {
+      if (deny.some(s => e.textContent.includes(s))) {
+        e.remove();
+      }
+    });
+    removeBuyNow();
+
+    const moreReleases = document.querySelector(".more-releases");
+    if (moreReleases?.offsetParent) {
+      moreReleases.click();
+    }
 
     if (Date.now() - startTime < 5000) {
-      requestAnimationFrame(check);
+      requestAnimationFrame(poll);
     }
   })();
 
@@ -39,16 +51,39 @@
     new MutationObserver(mutations => {
       for (const mut of mutations) {
         for (const node of mut.addedNodes) {
-          const deny = ["started following you", "bought", "pre-order"];
-
-          if (deny.some(s => node.innerText.includes(s))) {
+          if (deny.some(s => node.textContent.includes(s))) {
             node.remove();
           }
         }
       }
+
+      removeBuyNow();
     }).observe(document.querySelector("#story-list"), {
       childList: true, subtree: true, attributes: true
     });
   });
-})();
 
+  const style = document.createElement("style");
+  document.querySelector("head").appendChild(style);
+  style.textContent = `
+  .tralbum-art-large {
+    max-height: 120px;
+    max-width: 120px;
+  }
+
+  .tralbum-wrapper-collect-controls {
+    margin-bottom: 1em !important;
+  }
+
+  .play-button,
+  page-footer,
+  .less-releases,
+  .hear-more,
+  .buy-now,
+  .collection-item-fav-track,
+  .story-footer,
+  .collected-by,
+  .suggested-fans {
+    display: none !important;
+  }`;
+})();
